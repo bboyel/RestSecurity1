180420466b09d7b60c561ec5b03958cc7435df10
AleksandrErenkov <bboyel@mail.ru>
2020-04-05 12:55:49 +0400

    REST+SECURITY+PostgreSQL+JWT проект cоздан на start.spring.io

---
+++ src/main/java/net/erenkov/aleksandr/restsecurity1/RestSecurity1Application.java
@@ -0,0 +1,13 @@
+package net.erenkov.aleksandr.restsecurity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSecurity1Application.class, args);
+	}
+
+}
