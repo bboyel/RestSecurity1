794da07a4162c4d6d69650ebe46f664a87eb59df
AleksandrErenkov <bboyel@mail.ru>
2020-04-05 21:06:12 +0400

    Добавлен слой service, но без security не готово к использованию

---
+++ src/main/java/net/erenkov/aleksandr/restsecurity1/service/Impl/UserServiceImpl.java
@@ -0,0 +1,80 @@
+package net.erenkov.aleksandr.restsecurity1.service.Impl;
+
+import lombok.extern.slf4j.Slf4j;
+import net.erenkov.aleksandr.restsecurity1.model.Role;
+import net.erenkov.aleksandr.restsecurity1.model.Status;
+import net.erenkov.aleksandr.restsecurity1.model.User;
+import net.erenkov.aleksandr.restsecurity1.repository.RoleRepository;
+import net.erenkov.aleksandr.restsecurity1.repository.UserRepository;
+import net.erenkov.aleksandr.restsecurity1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j                  //Логирование ???
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public User register(User user) {
+        Role roleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> userRoles=new ArrayList<>();
+        userRoles.add(roleUser);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(userRoles);
+        user.setStatus(Status.ACTIVE);
+
+        User registeredUser = userRepository.save(user);
+        log.info("IN register - user: {} successfully registered", registeredUser );
+
+        return registeredUser;
+    }
+
+    @Override
+    public List<User> getAll() {
+        List<User> result = userRepository.findAll();
+        log.info("IN getAll - {} users found", result.size());
+        return result;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        User result = userRepository.findByUsername(username);
+        log.info("IN findByUsername - user: {} found by username : {}", result, username); //!!!Нужно обработать что пользователь не найден
+        return result;
+    }
+
+    @Override
+    public User findById(Long id) {
+        User result = userRepository.findById(id).orElse(null);         //!!!-если не найден
+
+        if(result==null){
+            log.warn("IN findById - no user found by id: {}", id);
+            return null;
+        }
+
+        log.info("IN findById - user: {} found by username : {}", result); //!!!Нужно обработать что пользователь не найден
+        return result;
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+        log.info("IN delete - user with id : {} successfully deleted", id);
+    }
+}
--- src/main/java/net/erenkov/aleksandr/restsecurity1/service/UserService.java
+++ src/main/java/net/erenkov/aleksandr/restsecurity1/service/UserService.java
@@ -1,4 +1,19 @@
 package net.erenkov.aleksandr.restsecurity1.service;
 
+import net.erenkov.aleksandr.restsecurity1.model.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    User register(User user);
+
+    List<User> getAll();
+
+    User findByUsername(String username);
+
+    User findById(Long id);
+
+    void delete(Long id);
+
 }
